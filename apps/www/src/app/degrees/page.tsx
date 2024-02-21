143b7836dbc010a06a30006e0c8708898899af5d
Andrew McGlathery <a.mcglathery@gmail.com>
2024-02-20 22:09:06 -0500

    [Draft] Course graph (#10)
    * got ReactFlow working
    
    * factor out DAG component
    
    * add dagre layout
    
    * whoops
    
    * add types for dagre
    
    * fix edge orintation and use the right data

--- apps/www/src/app/degrees/page.tsx
+++ apps/www/src/app/degrees/page.tsx
@@ -1,27 +1,28 @@
-import { StudentProfile } from "@/db/graph"
-
+import { CoursesGraph } from "@/components/courses-graph"
 import { ScheduleTable } from "@/components/schedule-table"
 
-export default async function Page() {
-  // const deptCourses = Object.keys(cseDegree.Courses).map((course): Prisma.CourseWhereInput => {
-  //   const [dept, num] = course.split(" ")
-  //   return {
-  //     department: {
-  //       code: dept
-  //     },
-  //     courseNumber: num
-  //   }
-  // })
+import { getDegree } from "./action"
 
-  // const requiredCourses = await db.course.findMany({
-  //   where: {
-  //     OR: deptCourses
-  //   },
-  //   select: {
-  //     id: true,
-  //     name: true
-  //   }
-  // })
+export default async function Page() {
+  //const deptCourses = Object.keys(cseDegree.Courses).map((course): Prisma.CourseWhereInput => {
+  //  const [dept, num] = course.split(" ")
+  //  return {
+  //    department: {
+  //      code: dept
+  //    },
+  //    courseNumber: num
+  //  }
+  //})
+
+  //const requiredCourses = await db.course.findMany({
+  //  where: {
+  //    OR: deptCourses
+  //  },
+  //  select: {
+  //    id: true,
+  //    name: true
+  //  }
+  //})
 
   // const cseProfile: StudentProfile = {
   //   requiredCourses: ["faec91f2-461b-4bb7-b266-cd1307ecae4d"], //requiredCourses.map(course => course.id),
@@ -29,22 +30,24 @@ export default async function Page() {
   //   timeToGraduate: 8
   // }
 
-  // const calc2 = "faec91f2-461b-4bb7-b266-cd1307ecae4d"
-  // const calc1 = "2e6b393e-1b5c-4a46-a5be-f62e41545748"
+  const calc2 = "faec91f2-461b-4bb7-b266-cd1307ecae4d"
+  const calc1 = "2e6b393e-1b5c-4a46-a5be-f62e41545748"
   const calc3 = "e4ada3c1-f89a-48c6-bbcd-3a6165fce77d"
-  // const precalc = "6b15a066-a434-499b-8b26-6179ff2dca19"
+  const precalc = "6b15a066-a434-499b-8b26-6179ff2dca19"
 
-  const mathProfile: StudentProfile = {
-    requiredCourses: [calc3],
+  const profile = {
+    requiredCourses: [calc1, calc2, calc3, precalc],
     completedCourses: [],
     timeToGraduate: 6
   }
 
+  const graph = await getDegree(profile)
+
   return (
     <div className="p-4">
       <h1 className="text-3xl font-bold">Degree</h1>
-
-      <ScheduleTable profile={mathProfile} />
+      <ScheduleTable profile={profile} />
+      <CoursesGraph graph={graph} />
     </div>
   )
 }
