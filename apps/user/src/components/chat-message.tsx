dd0f9dbed51107ce0404370f9b18ea80df2d9ecd
Ethan Leifer <ethanleifer@gmail.com>
2024-02-28 16:10:01 -0500

    refactor out advisor chat stuff

--- apps/user/src/components/chat-message.tsx
+++ apps/user/src/components/chat-message.tsx
@@ -1,28 +1,12 @@
-import React, { ReactNode } from "react"
-import { AdvisorAgent, advisorAgentSchema } from "@/agents/advisor/schema"
+import React from "react"
+import { advisorAgentSchema } from "@/agents/advisor/schema"
 import { User } from "@db/client"
 import { StudentProfile } from "@graph/types"
 import { cn } from "@ui/lib/utils"
 
+import { AdvisorMessageBody } from "./advisor-chat"
 import { CustomMessage } from "./chat"
 import { MdxContent } from "./mdxContent"
-import { CourseDisplay } from "./messageHandlers/course-display"
-import { RescheduleCourse } from "./messageHandlers/reschedule-course"
-import { ScheduleTable } from "./messageHandlers/schedule-table"
-import { SemesterDisplay } from "./messageHandlers/semester-display"
-
-type ChatMessageSubType = AdvisorAgent["advisor_output"]["actions"][number]["type"]
-
-type HandleChatMessage<T extends ChatMessageSubType> = (params: {
-  studentProfile: StudentProfile
-  setStudentProfile: (profile: StudentProfile) => void
-  student: User
-  action: Extract<AdvisorAgent["advisor_output"]["actions"][number], { type: T }>
-}) => ReactNode
-
-type ChatMessageHandler = {
-  [K in ChatMessageSubType]: HandleChatMessage<K>
-}
 
 type ChatMessageParams = {
   partial?: boolean
@@ -31,37 +15,6 @@ type ChatMessageParams = {
   setStudentProfile: (profile: StudentProfile) => void
   message: CustomMessage
 }
-
-const chatMessageHandler: ChatMessageHandler = {
-  "4-year-plan": ({ studentProfile }) => {
-    return <ScheduleTable profile={studentProfile} />
-  },
-  "display-course": ({ action, studentProfile }) => {
-    return <CourseDisplay course={action.course_name} profile={studentProfile} />
-  },
-  "error": ({ action }) => {
-    return <p className="text-red-500">{`Assistant Error: ${action.error}`}</p>
-  },
-  "rescheduleCourse": ({ action, studentProfile, setStudentProfile }) => {
-    return (
-      <RescheduleCourse
-        courseName={action.course_name}
-        toSemester={action.toSemester}
-        profile={studentProfile}
-        setProfile={setStudentProfile}
-      />
-    )
-  },
-  "display-semester": ({ action: advisor_output, studentProfile }) => {
-    return (
-      <div>
-        <strong>Assistant:</strong> {`Displaying semester ${advisor_output.semester}`}
-        <SemesterDisplay semester={advisor_output.semester - 1} profile={studentProfile} />
-      </div>
-    )
-  }
-}
-
 export const ChatMessage = ({
   partial = false,
   message,
@@ -143,39 +96,3 @@ export const MessageWrapper = React.memo(function MessageWrapper({
     </div>
   )
 })
-
-const AdvisorMessageBody = ({
-  advisor_output,
-  ...params
-}: {
-  advisor_output: AdvisorAgent["advisor_output"]
-  studentProfile: StudentProfile
-  setStudentProfile: (profile: StudentProfile) => void
-  student: User
-}) => {
-  return (
-    <>
-      <MdxContent content={advisor_output.response} />
-      <small className="text-gray-500">
-        {advisor_output.actions?.map(action => action.type).join(", ")}
-      </small>
-
-      <div>
-        {advisor_output?.actions?.map((action, i) => {
-          const Handler = chatMessageHandler[action.type]
-
-          return (
-            <Handler
-              key={i}
-              // @ts-ignore
-              action={action}
-              studentProfile={params.studentProfile}
-              setStudentProfile={params.setStudentProfile}
-              student={params.student}
-            />
-          )
-        })}
-      </div>
-    </>
-  )
-}
