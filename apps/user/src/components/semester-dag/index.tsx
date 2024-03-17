0f4a2b611902b3ad42a6cf6d7a52b568d32aea33
Ethan Leifer <ethanleifer@gmail.com>
2024-03-17 17:31:26 -0600

    add edges to nodes before transfer credits

--- apps/user/src/components/semester-dag/index.tsx
+++ apps/user/src/components/semester-dag/index.tsx
@@ -1,9 +1,11 @@
-import { StudentProfile } from "@graph/types"
+import { StudentProfile, CourseNode } from "@graph/types"
 import { DAG } from "@ui/components/dag"
 import { Edge, Node } from "reactflow"
 
 import { CourseNodeType, CourseNode as CustomCourseNode } from "./course-node"
 import { SemesterNode, SemesterNodeType } from "./semester-node"
+import { getCourseWithPrereqs } from "@/db/courses"
+import { boolean } from "zod"
 
 const defaultSemesterNode: Partial<Node> = {
 	draggable: false,
@@ -30,11 +32,10 @@ const defaultCourseNode: Partial<Node> = {
 	// extent: 'parent'
 }
 
-export function SemesterDAG({
-	studentProfile: { graph, semesters, transferCredits, allCourses }
-}: {
-	studentProfile: StudentProfile
-}) {
+function getSemesterNodesAndEdges(
+	semesters: CourseNode[][],
+	allCourses: CourseNode[]
+) {
 	const nodes: Node[] = []
 	const parentNodes: SemesterNodeType[] = semesters.map((_semester, index) => {
 		return {
@@ -52,7 +53,7 @@ export function SemesterDAG({
 			semester.map((course, courseIndex): Node => {
 				return {
 					...defaultCourseNode,
-					id: `${course.id}`,
+					id: course.id,
 					parentNode: `semester-${semIndex}`,
 					position: { x: 0, y: 50 + courseIndex * 50 },
 					data: { semesterIndex: semIndex + 1, ...course }
@@ -66,8 +67,6 @@ export function SemesterDAG({
 	const edges: Edge[] = allCourses.flatMap(course => {
 		return (
 			course.prerequisites
-				//we only need to render the edges when the prereq course is also going to be rendered
-				.filter(prereq => [...graph.values()].find(node => node.id === prereq))
 				.map(prereq => {
 					return {
 						id: `${prereq}-${course.id}`,
@@ -80,53 +79,147 @@ export function SemesterDAG({
 		)
 	})
 
-	if (transferCredits.length > 0) {
-		nodes.push({
-			...defaultSemesterNode,
-			id: "transfer",
-			position: { x: -200, y: 0 },
-			data: { transfer: true }
-		})
+	return { nodes, edges }
+}
 
-		const transferNodes: CourseNodeType[] = transferCredits.map((c, i) => {
+const getTransferNodesAndEdges = (
+	transferCredits: string[],
+	graph: Map<string, CourseNode>
+) => {
+	const nodes: Node[] = []
+	const edges: Edge[] = []
+
+	nodes.push({
+		...defaultSemesterNode,
+		id: "transfer",
+		position: { x: -200, y: 0 },
+		data: { transfer: true }
+	})
+
+	const transferNodes: CourseNodeType[] = transferCredits.map((c, i) => {
+		const n = graph.get(c)
+		if (!n) {
+			throw new Error(`Could not find course with id ${c}`)
+		}
+
+		return {
+			...defaultCourseNode,
+			id: n.id,
+			parentNode: "transfer",
+			position: { x: 0, y: 50 + i * 50 },
+			data: { semesterIndex: -1, ...n }
+		}
+	})
+
+	nodes.push(...transferNodes)
+
+	const transferEdges: Edge[] = transferCredits
+		.map(credit => {
+			const n = graph.get(credit)
+
+			if (!n) {
+				throw new Error(`Could not find course with id ${credit}`)
+			}
+
+			return n.dependents.map((dep, i) => {
+				return {
+					id: `${n.id}-${dep}-${i}`,
+					type: "default",
+					source: n.id,
+					target: dep,
+					zIndex: 2
+				}
+			})
+		})
+		.flat()
+
+	edges.push(...transferEdges)
+
+	return { nodes, edges }
+}
+
+const getUnassignedNodesAndEdges = async (graph: Map<string, CourseNode>, nodes: Node[], transferCredits: CourseNode[]) => {
+	const coursesNotInSemesterOrTransferNode = [...graph.values()].filter(({ id }) => !nodes.map(n => n.id).includes(id))
+
+	// console.log(coursesNotInSemesterOrTransferNode)
+	const unassignedNodes: CourseNodeType[] = coursesNotInSemesterOrTransferNode.map((course, i) => {
+		return {
+			...defaultCourseNode,
+			id: course.id,
+			position: { x: -400 - (200 * i), y: 50 },
+			data: { semesterIndex: -1, ...course },
+			style: {
+				...defaultCourseNode.style,
+				backgroundColor: "lightgrey"
+			}
+		}
+	})
+
+
+	const prereqs = await Promise.all([...coursesNotInSemesterOrTransferNode, ...transferCredits].map(async course => {
+		return {
+			id: course.id, prereqs: (await getCourseWithPrereqs(course.id, [])).prereqMap.get(course.id)?.filter(Boolean) ?? []
+		}
+	}))
+
+	const unassignedEdges: Edge[] = prereqs.flatMap(({ id, prereqs }) => {
+
+		return (
+			prereqs.map(prereq => {
+				return {
+					id: `${prereq}-${id}`,
+					type: "default",
+					source: prereq,
+					target: id,
+					zIndex: 2
+				}
+			})
+		)
+	})
+
+	return { nodes: unassignedNodes, edges: unassignedEdges }
+}
+
+
+export async function SemesterDAG({
+	studentProfile: { graph, semesters, transferCredits, allCourses }
+}: {
+	studentProfile: StudentProfile
+}) {
+
+	// first we want to render the semesters, courses and their edges
+	const { nodes, edges } = getSemesterNodesAndEdges(semesters, allCourses)
+
+	// if there are transfer credits, we want to render them and their edges
+	if (transferCredits.length > 0) {
+		const transferNodesAndEdges = getTransferNodesAndEdges(transferCredits, graph)
+
+		nodes.push(...transferNodesAndEdges.nodes)
+		edges.push(...transferNodesAndEdges.edges)
+	}
+
+	// we also want to display all nodes not in transfer or a semester and its edges
+
+	const { nodes: unassignedNodes, edges: unassignedEdges } = await getUnassignedNodesAndEdges(graph, nodes, transferCredits.map(
+		c => {
 			const n = graph.get(c)
 			if (!n) {
 				throw new Error(`Could not find course with id ${c}`)
 			}
+			return n
+		}
+	))
 
-			return {
-				...defaultCourseNode,
-				id: n.id,
-				parentNode: "transfer",
-				position: { x: 0, y: 50 + i * 50 },
-				data: { semesterIndex: -1, ...n }
-			}
-		})
+	// // console.log(unassignedCourses.map(course => ({ id: course.id, preq: course.prerequisites })))
 
-		nodes.push(...transferNodes)
+	// // console.log(await getCourseWithPrereqs(unassignedCourses.map(course => course.id)[0] ?? "", []))
+	console.log(unassignedEdges)
 
-		const transferEdges: Edge[] = transferCredits
-			.map(credit => {
-				const n = graph.get(credit)
+	nodes.push(...unassignedNodes)
+	edges.push(...unassignedEdges)
 
-				if (!n) {
-					throw new Error(`Could not find course with id ${credit}`)
-				}
 
-				return n.dependents.map((dep, i) => {
-					return {
-						id: `${n.id}-${dep}-${i}`,
-						type: "default",
-						source: n.id,
-						target: dep,
-						zIndex: 2
-					}
-				})
-			})
-			.flat()
 
-		edges.push(...transferEdges)
-	}
 
 	return (
 		<div style={{ width: "100vw", height: "90vh" }}>
