20c56fe170d9a48e358cf3e20691b4b2d48540c0
Ethan Leifer <ethanleifer@gmail.com>
2024-03-24 15:53:13 -0400

    refactor dag to make it easier

--- apps/user/src/components/semester-dag/index.tsx
+++ apps/user/src/components/semester-dag/index.tsx
@@ -1,58 +1,105 @@
-import { StudentProfile } from "@repo/graph/types"
+"use client"
 
-import { CourseNode as CustomCourseNode } from "./course-node"
-import {
-  getSemesterNodesAndEdges,
-  getTransferNodesAndEdges,
-  getUnassignedNodesAndEdges
-} from "./graph-to-node-utils"
-import { SemesterDagChat } from "./semester-dag-chat"
+import { PromptComposer } from "@ui/components/prompt-composer"
+import ReactFlow, {
+  Background,
+  Controls,
+  Edge,
+  MiniMap,
+  Node,
+  NodeProps,
+  useEdgesState,
+  useNodesState
+} from "reactflow"
+
+import "reactflow/dist/style.css"
+
+import { useJsonStream } from "stream-hooks"
+import { z } from "zod"
+
+import { CourseNode, CourseNodeType } from "./course-node"
 import { SemesterNode } from "./semester-node"
 
-export async function SemesterDAG({
-  studentProfile: { graph, semesters, transferCredits, allCourses }
+export function SemesterDAG({
+  nodes: initialNodes,
+  edges: initialEdges
 }: {
-  studentProfile: StudentProfile
+  nodes: Node[]
+  edges: Edge[]
 }) {
-  // first we want to render the semesters, courses and their edges
-  const { nodes: defaultNodes, edges: defaultEdges } = getSemesterNodesAndEdges(
-    semesters,
-    allCourses
-  )
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
 
-  // if there are transfer credits, we want to render them and their edges
-  if (transferCredits.length > 0) {
-    const transferNodesAndEdges = getTransferNodesAndEdges(transferCredits, graph)
+  const { startStream, loading } = useJsonStream({
+    schema: z.object({
+      nodes: z.array(z.custom<CourseNodeType>()),
+      edges: z.array(z.custom<Edge>())
+    }),
+    onEnd: async ({ nodes, edges }) => {
+      setNodes(prev => [...prev, ...nodes])
+      setEdges(prev => [...prev, ...edges])
+    }
+  })
 
-    defaultNodes.push(...transferNodesAndEdges.nodes)
-    defaultEdges.push(...transferNodesAndEdges.edges)
+  const onSubmit = function (value: string) {
+    startStream({
+      url: "/api/ai/dag-chat",
+      body: {
+        messages: [
+          {
+            role: "user",
+            content: value
+          }
+        ],
+        electives: [
+          {
+            courseSubject: "CAP",
+            courseNumber: "4034",
+            name: "Computer Animation Fundamentals"
+          },
+          {
+            courseNumber: "4103",
+            courseSubject: "CAP",
+            name: "Mobile Biometrics"
+          }
+        ],
+        prevNodes: nodes
+      }
+    })
   }
 
-  // we also want to display all nodes not in transfer or a semester and its edges
-
-  const { nodes: unassignedNodes, edges: unassignedEdges } = await getUnassignedNodesAndEdges(
-    graph,
-    defaultNodes,
-    transferCredits.map(c => {
-      const n = graph.get(c)
-      if (!n) {
-        throw new Error(`Could not find course with id ${c}`)
-      }
-      return n
-    })
-  )
-
-  defaultNodes.push(...unassignedNodes)
-  defaultEdges.push(...unassignedEdges)
-
   return (
-    <SemesterDagChat
-      customNodes={{
-        semesterNode: SemesterNode,
-        courseNode: CustomCourseNode
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center"
       }}
-      nodes={defaultNodes}
-      edges={defaultEdges}
-    />
+    >
+      <ReactFlow
+        nodeTypes={{
+          semesterNode: SemesterNode,
+          courseNode: CourseNode
+        }}
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+      >
+        <Background />
+        <MiniMap />
+        <Controls />
+      </ReactFlow>
+      <div className="w-full p-2">
+        <PromptComposer
+          placeholder="What courses should I add?"
+          onSubmit={onSubmit}
+          loading={loading}
+        />
+      </div>
+    </div>
   )
 }
