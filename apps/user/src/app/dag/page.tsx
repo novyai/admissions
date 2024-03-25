c6ba5f4b666d680e8c9ea9ca620c5d86f66299a0
Ethan Leifer <ethanleifer@gmail.com>
2024-03-25 15:09:28 -0400

    setup base versioning

--- apps/user/src/app/dag/page.tsx
+++ apps/user/src/app/dag/page.tsx
@@ -1,8 +1,9 @@
 import { redirect } from "next/navigation"
+import { getVersions } from "@/db/versions"
 import { auth, UserButton } from "@clerk/nextjs"
 import { Button } from "@ui/components/ui/button"
 import { Tooltip, TooltipContent, TooltipTrigger } from "@ui/components/ui/tooltip"
-import { LifeBuoy, SquareTerminal } from "lucide-react"
+import { LifeBuoy } from "lucide-react"
 
 import { Novy } from "@/components/novy-logo"
 import { TemporaryDag } from "@/components/semester-dag/temp-dag"
@@ -18,92 +19,16 @@ export default async function Page() {
     redirect("/")
   }
 
-  return <Dashboard />
-}
-
-function Dashboard() {
   return (
     <div className="flex h-screen w-screen flex-row ">
       <div className="flex h-full flex-col border-r">
-        <div className="border-b h-[60px] flex items-center justify-center">
-          <Button variant="outline" size="icon" aria-label="Home">
+        <div className="border-b gap-2 h-[60px] flex items-center justify-center">
+          <Button variant="outline" aria-label="Home" className="flex">
             <Novy width={32} height={32} />
+            <h1 className="text-xxl font-semibold">Playground</h1>
           </Button>
         </div>
-        <div className="grid gap-1 p-2">
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                variant="ghost"
-                size="icon"
-                className="rounded-lg border"
-                aria-label="Playground"
-              >
-                <SquareTerminal className="size-5" />
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent side="right" sideOffset={5}>
-              Playground
-            </TooltipContent>
-          </Tooltip>
-        </div>
-        {/* <nav className="grid gap-1 p-2">
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                variant="ghost"
-                size="icon"
-                className="rounded-lg bg-muted"
-                aria-label="Playground"
-              >
-                <SquareTerminal className="size-5" />
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent side="right" sideOffset={5}>
-              Playground
-            </TooltipContent>
-          </Tooltip>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button variant="ghost" size="icon" className="rounded-lg" aria-label="Models">
-                <Bot className="size-5" />
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent side="right" sideOffset={5}>
-              Models
-            </TooltipContent>
-          </Tooltip>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button variant="ghost" size="icon" className="rounded-lg" aria-label="API">
-                <Code2 className="size-5" />
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent side="right" sideOffset={5}>
-              API
-            </TooltipContent>
-          </Tooltip>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button variant="ghost" size="icon" className="rounded-lg" aria-label="Documentation">
-                <Book className="size-5" />
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent side="right" sideOffset={5}>
-              Documentation
-            </TooltipContent>
-          </Tooltip>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button variant="ghost" size="icon" className="rounded-lg" aria-label="Settings">
-                <Settings2 className="size-5" />
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent side="right" sideOffset={5}>
-              Settings
-            </TooltipContent>
-          </Tooltip>
-        </nav> */}
+        <DagVersionSidebar userId={userId} />
         <nav className="mt-auto grid gap-1 p-2">
           <Tooltip>
             <TooltipTrigger asChild>
@@ -119,14 +44,9 @@ function Dashboard() {
       </div>
       <div className="flex flex-col w-full h-full">
         <header className="sticky top-0 z-10 flex h-[60px] items-center gap-1 border-b bg-background px-4">
-          <h1 className="text-xl font-semibold">Playground</h1>
           <div className="ml-auto">
             <UserButton afterSignOutUrl="/" />
           </div>
-          {/* <Button variant="outline" size="sm" className="ml-auto gap-1.5 text-sm">
-            <Share className="size-3.5" />
-            Share
-          </Button> */}
         </header>
         <main className="grid flex-1 gap-4 overflow-auto p-4">
           <DagChat />
@@ -140,6 +60,23 @@ const DagChat = () => {
   return (
     <div className="relative flex h-full min-h-[50vh] flex-col rounded-xl border p-4 lg:col-span-2">
       <TemporaryDag />
+    </div>
+  )
+}
+
+async function DagVersionSidebar({ userId }: { userId: string }) {
+  const versions = await getVersions(userId)
+
+  if (!versions.length) {
+    // this should never be the case, cause on user creation we should create a first version
+    return <p>No versions</p>
+  }
+
+  return (
+    <div className="flex flex-col gap-2 p-2">
+      {versions.map(version => {
+        return <div key={version.id}>{version.id}</div>
+      })}
     </div>
   )
 }
