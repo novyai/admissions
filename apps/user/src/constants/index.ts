45566e9038cd42fbfb1062278ccf5033ea56c878
Dimitri Kennedy <dimitrikennedy@gmail.com>
2024-04-12 12:35:07 -0400

    fix lock

--- apps/user/src/constants/index.ts
+++ apps/user/src/constants/index.ts
@@ -5,14 +5,10 @@ const isLocal = true //process?.env?.["NEXT_PUBLIC_IS_LOCAL"] === "1"
 const isQA = true //process?.env?.["NEXT_PUBLIC_RW_ENV"] === "QA"
 
 export const OLMPA_BASE_URL =
-  isQA ?
-    "admissions-olmpa-api-production.up.railway.app"
-  : "admissions-olmpa-api-production.up.railway.app"
+  isQA ? "admissions-olmpa-production.up.railway.app" : "admissions-olmpa-production.up.railway.app"
 
 export const WS_BASE_URL =
-  isQA ?
-    "admissions-ws-api-production.up.railway.app"
-  : "admissions-ws-api-production.up.railway.app"
+  isQA ? "asmissions-ws-production.up.railway.app" : "asmissions-ws-production.up.railway.app"
 
 export const SOCKET_BASE_URL = isLocal ? `ws://localhost:6970/ws` : `wss://${WS_BASE_URL}/ws`
 export const Q_BASE_URL = isLocal ? "http://localhost:6969/q" : `https://${OLMPA_BASE_URL}/q`
