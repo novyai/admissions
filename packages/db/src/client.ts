b8b3701bcc3f5f15781914b4bb3fc29545e1c451
Ethan Leifer <ethanleifer@gmail.com>
2024-05-03 10:24:42 -0400

    checkpoint

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,12 +1,6 @@
 import { Prisma, PrismaClient } from "@prisma/client"
 
-declare global {
-  var db: PrismaClient | undefined
-}
-
-export const db = global.db || new PrismaClient()
-
-if (process.env.NODE_ENV !== "production") global.db = db
+export const db = new PrismaClient()
+export * from "@prisma/client"
 
 export default Prisma
-export * from "@prisma/client"
